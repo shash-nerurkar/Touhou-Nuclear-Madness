7a97907f43a17461bc0088f0dc7c17a1b8bdb63e
shash-nerurkar <shash.nerurkar@gmail.com>
2024-01-15 07:17:43 +0530

    adds fix for player hitbox not working properly

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -123,28 +123,12 @@ public class Bullet : MonoBehaviour
     }
 
     private void OnTriggerEnter2D ( Collider2D collided ) {
+        int collidedLayer = collided.gameObject.layer;
+        if ( collidedLayer == LayerMask.NameToLayer ( Constants.COLLISION_LAYER_PLAYER_GRAZE ) )
+            return;
+        
         cd.enabled = false;
 
-        int collidedLayer = collided.gameObject.layer;
-        if ( collidedLayer == LayerMask.NameToLayer ( Constants.COLLISION_LAYER_SCREEN_BORDER ) ) {
-            OnHitAnimationComplete ( );
-        }
-        else if ( collidedLayer == LayerMask.NameToLayer ( Constants.COLLISION_LAYER_PLAYER ) ) {        
-            collided.GetComponent<Player> ( ).TakeDamage ( damage: damage );
-            
-            OnHit ( );
-        }
-        else if ( collidedLayer == LayerMask.NameToLayer ( Constants.COLLISION_LAYER_ENEMY ) ) {        
-            collided.GetComponent<Enemy> ( ).TakeDamage ( damage: damage );
-            
-            OnHit ( );
-        }
-    }
-
-    private void OnTriggerStay2D ( Collider2D collided ) {
-        cd.enabled = false;
-
-        int collidedLayer = collided.gameObject.layer;
         if ( collidedLayer == LayerMask.NameToLayer ( Constants.COLLISION_LAYER_SCREEN_BORDER ) ) {
             OnHitAnimationComplete ( );
         }
